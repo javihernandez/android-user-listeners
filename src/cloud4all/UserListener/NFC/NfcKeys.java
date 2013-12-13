7d9211a8729af1917bc680a70fb117a905cb7601
Javier Hernández <jhernandez@emergya.com>
2013-12-13 10:56:35 +0100

    Adding headers with license and Cloud4all's funding acknowledgement
    Changing encoding of files to unix/linux

--- src/cloud4all/UserListener/NFC/NfcKeys.java
+++ src/cloud4all/UserListener/NFC/NfcKeys.java
@@ -1,3 +1,20 @@
+/**
+ * android-user-listeners
+ *  ----------------------
+ *
+ * Copyright 2013 UPM
+ * Copyright 2013 Emergya
+ *
+ * Licensed under the New BSD license. You may not use this file except in
+ * compliance with this License.
+ *
+ * You may obtain a copy of the License at
+ * https://github.com/gpii/universal/LICENSE.txt
+ *
+ * The research leading to these results has received funding from the European
+ * Union's Seventh Framework Programme (FP7/2007-2013) under grant agreement 
+ * #289016.
+ **/
 package cloud4all.UserListener.NFC;
 
 public class NfcKeys
